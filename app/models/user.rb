f2060213a4bb33f151e287625b9830eed8ff154f
ueda-takahiro <ueda196@gmail.com>
2019-10-23 15:38:38 +0900

    reinstall devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-      #  belongs_to :nickname
-      has_many :personality
-      has_many :fireworks_name
-      has_many :scores, through: :user_scores
+       #belongs_to :nickname 
+       has_many :personality
+       has_many :fireworks_name
+       has_many :scores, through: :user_scores
 end
