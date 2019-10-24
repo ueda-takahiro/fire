589c0696bbe6d8466e450a803d95e739f217138c
ueda-takahiro <ueda196@gmail.com>
2019-10-25 00:56:32 +0900

    fix migration-fire

--- db/migrate/20191002100312_create_user_scores.rb
+++ db/migrate/20191002100312_create_user_scores.rb
@@ -1,8 +1,8 @@
 class CreateUserScores < ActiveRecord::Migration[5.2]
   def change
     create_table :user_scores do |t|
-      t.integer :score, foreign_key: true
-      t.integer :user, foreign_key: true
+      t.integer :score_id, foreign_key: true
+      t.integer :user_id, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -46,8 +46,8 @@ ActiveRecord::Schema.define(version: 2019_10_24_094203) do
   end
 
   create_table "user_scores", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "score"
-    t.integer "user"
+    t.integer "score_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
