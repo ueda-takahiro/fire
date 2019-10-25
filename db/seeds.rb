e7f7bed368ab8a65aee0d600b2d1f6cb58eed608
ueda-takahiro <ueda196@gmail.com>
2019-10-25 21:25:32 +0900

    fix seed-fire

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-
-#親
-Personality.create(point: "男")
-Personality.create(point: "女")
-Personality.create(point: "おしゃれ")
-Personality.create(point: "激しい")
-Personality.create(point: "かっこいい")
-Personality.create(point: "おっとり")
-Personality.create(point: "儚い")
-Personality.create(point: "派手")
