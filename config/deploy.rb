d926caf4f118490aafac4ae1783f25c188de95a0
ueda-takahiro <ueda196@gmail.com>
2019-10-31 13:38:48 +0900

    fix routeing

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.2"
 
 set :application, "fire"
-set :repo_url, "git@example.com/ueda-takahiro/fire.git"
+set :repo_url, "git@github.com:ueda-takahiro/fire.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log','tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 # バージョンが変わっても共通で参照するディレクトリを指定
@@ -11,7 +11,7 @@ set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/techair56.pem']  ※例：~/.ssh/key_pem.pem
+                  keys: ['~/.ssh/techair56.pem']
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
