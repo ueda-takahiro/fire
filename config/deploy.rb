e25ddba208da9f75f466ccc68c2bb173cca30b51
ueda-takahiro <ueda196@gmail.com>
2019-11-06 21:19:43 +0900

    refix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,7 +41,14 @@ end
 
 # Default value for :pty is false
 # set :pty, true
-
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"],
+  BASIC_AUTH_USER: ENV["BASIC_AUTH_USER"],
+  BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"]
+}
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
 
